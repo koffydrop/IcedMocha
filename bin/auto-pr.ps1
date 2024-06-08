0e8e8a181d422ead7653c54f9b52d3d2560b571d
Kira <67888720+kira-nyx@users.noreply.github.com>
2024-06-08 00:47:28 -0300

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "kira-nyx/scoop-personal:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
